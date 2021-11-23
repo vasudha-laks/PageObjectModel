f6e1cf1aec5cef578fee8685c7010b1b112a2667
vasudha-laks <vasudhalakshmi.koka@gmail.com>
2021-11-23 15:19:15 +0530

    some message

--- src/test/java/com/crm/qa/testcases/Vasuda_auto.java
+++ src/test/java/com/crm/qa/testcases/Vasuda_auto.java
@@ -3,8 +3,7 @@ package com.crm.qa.testcases;
 public class Vasuda_auto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("HelloWorld");
 	}
 
 }
